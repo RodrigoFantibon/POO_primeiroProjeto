7ae2c2d5d1098bf3b82ff487c4ceba0794c34ef2
RodrigoFantibon <rodrigo.fantibon@hotmail.com>
2021-08-26 17:27:25 -0300

    exercicio modificado final, utilizando getters e setters

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -1,12 +1,19 @@
     public class LivroDeNotas {
 
-        String nomeDoCurso;
+       
+        private String nomeCurso;
 
+        public void setNomeCurso (String s){
+            nomeCurso = s;
+        }
+        public String getNomeCurso(){
+            return nomeCurso;
+        }
 
 
         public void ExibirMensagem (){
             //System.out.println("Bem vindo ao seu livro de notas");
-            System.out.printf("bem vindo ao livro de notas do curso %s!", nomeDoCurso);
+            System.out.printf("bem vindo ao livro de notas do curso %s!", getNomeCurso());
 
         }
 
--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -1,21 +1,12 @@
-import javax.swing.JOptionPane;
 
 public class TesteLivroDeNotas {
 
 public static void main(String[] args) {
     
-    LivroDeNotas livroDeNotas; // poderia ser qualquer um o nome da variavel
-    livroDeNotas = new LivroDeNotas();
-    String nomeDoCurso = JOptionPane.showInputDialog("digite o nome do curso"); //poderia ser qualquer um o nome da variavel
-    livroDeNotas.nomeDoCurso = nomeDoCurso;
-    livroDeNotas.ExibirMensagem();
+   LivroDeNotas livro = new LivroDeNotas();
+   livro.setNomeCurso ("ADS");
+   System.out.println(livro.getNomeCurso());
+   livro.ExibirMensagem();
 
-    LivroDeNotas outroLivro = new LivroDeNotas();
-    String outroNome = JOptionPane.showInputDialog("Nome do segundo curso");
-    outroLivro.nomeDoCurso = outroNome;
-    outroLivro.ExibirMensagem();
-
-    System.out.println("terminado...");
-    }
-   
+}
 }
